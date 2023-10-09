c1e3b958c443c96666e224fdb9e647fa190c3887
常袭 <2822776181@qq.com>
2023-10-09 17:26:20 +0800

    Initial commit

---
+++ src/main/java/com/cxi/CxiFirstModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.cxi;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CxiFirstModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
